58ca996947c4c823cd668520b1d3f75e2577d0bd
mgonzalezlorenzo <mgonzalezlorenzo@atanasoff05.danielcastelao.org>
2016-06-09 09:10:05 +0200

    Variables introducidas por consola

--- src/busqueda/Busqueda.java
+++ src/busqueda/Busqueda.java
@@ -1,15 +1,25 @@
 package busqueda;
 
+import java.util.Scanner;
+
 public class Busqueda {
 
     public static void main(String[] args) {
-        int[] numeros = new int[100];
+        Scanner objetoConsola=new Scanner(System.in);
+        int longArray = 0;
+        System.out.println("Introduzca la longitud del array");
+        longArray = objetoConsola.nextInt();
+        int valor=0;
+        System.out.println("Introduzca el numero del que adivinar el indice");
+        valor =  objetoConsola.nextInt();
+        int[] numeros = new int[longArray];
         for (int i = 0; i < numeros.length; i++) {
             numeros[i] = i * 5;
         }
+        
         Busqueda ejemplo = new Busqueda();
-        int indice = ejemplo.busquedaBinaria(numeros, 450, 0, numeros.length - 1);
-        System.out.println("El indice del valor '450' es: " + indice);
+        int indice = ejemplo.busquedaBinaria(numeros, valor, 0, numeros.length - 1);
+        System.out.println("El indice del valor '"+valor+"' es: " + indice); 
     }
 
     public int busquedaBinaria(int[] listado, int clave, int posInicial, int posFinal) {
